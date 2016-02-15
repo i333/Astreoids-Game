2ec29c63a89f9dfc1f304691358fb58bbb9f3a43
Utku Dora <utku.ozdemir@pisano.co>
2016-02-15 13:58:25 -0500

    Initial commit

---
+++ Astroids/Astroids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astroids
+//
+//  Created by Utku Dora on 15/02/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Astroids/Astroids/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Astroids
+//
+//  Created by Utku Dora on 15/02/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
